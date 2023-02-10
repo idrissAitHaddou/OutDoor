d9e3c27ea424b9af20595c80b0e537674a47ff77
Mohamed <mrbalizi52@gmail.com>
2023-02-10 15:08:13 +0100

    create entities

---
+++ product/src/main/java/com/outdoor/product/repository/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.outdoor.product.repository;
+
+import com.outdoor.product.entities.Product;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+public interface ProductRepository extends ElasticsearchRepository<Product,Long> {
+}
