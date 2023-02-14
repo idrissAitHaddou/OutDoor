172dc71da745f6ea4290aec8be49ae037d2193cc
idriss <idrissaithadou@gmail.com>
2023-02-14 16:18:02 +0100

    added gatewaye

--- client/src/main/java/com/outdoor/client/ClientApplication.java
+++ client/src/main/java/com/outdoor/client/ClientApplication.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-
 @SpringBootApplication
 @EnableDiscoveryClient
 public class ClientApplication {
--- client/src/main/java/com/outdoor/client/entities/User.java
+++ client/src/main/java/com/outdoor/client/entities/User.java
@@ -12,17 +12,10 @@ import org.springframework.data.mongodb.core.mapping.Document;
 public class User {
     @Id
     private String id_user;
-
     private String firstname;
-
     private String lastname;
-
     private String email;
-
     private String password;
-
     private String image;
-
     private String role;
-
 }
