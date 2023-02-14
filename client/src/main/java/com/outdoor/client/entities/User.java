52ecf6b2e701e20d2531b132faae8b352be0947a
idriss <idrissaithadou@gmail.com>
2023-02-14 09:48:19 +0100

    added aeureka server

--- client/src/main/java/com/outdoor/client/entities/User.java
+++ client/src/main/java/com/outdoor/client/entities/User.java
@@ -4,12 +4,7 @@ import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-
-<<<<<<< HEAD
 @Document(collection = "users")
-=======
-@Document(collection = "user")
->>>>>>> ae0533eb34d705c91c38186ac3a9e9f1e9eed94a
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
@@ -29,11 +24,5 @@ public class User {
     private String image;
 
     private String role;
-<<<<<<< HEAD
-=======
-    
 
-
-
->>>>>>> ae0533eb34d705c91c38186ac3a9e9f1e9eed94a
 }
