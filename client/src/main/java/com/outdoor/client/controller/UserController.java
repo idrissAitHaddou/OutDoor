4304267068cff99904a95078a7f4a09a5a4b3ac4
Mohamed <mrbalizi52@gmail.com>
2023-02-14 16:22:43 +0100

    gateway

--- client/src/main/java/com/outdoor/client/controller/UserController.java
+++ client/src/main/java/com/outdoor/client/controller/UserController.java
@@ -1,24 +1,26 @@
 package com.outdoor.client.controller;
 
-import com.outdoor.client.entities.User;
 import com.outdoor.client.request.UserRequest;
 import com.outdoor.client.response.UserResponse;
 import com.outdoor.client.services.UserService;
 import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequiredArgsConstructor
 @RequestMapping("/api/v1/user")
 public class UserController {
 
     private final UserService userService;
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
-//    create user
+    //    create user
     @PostMapping("/create")
     @ResponseStatus(HttpStatus.CREATED)
     public void createUser(@Valid @RequestBody UserRequest userRequest){
